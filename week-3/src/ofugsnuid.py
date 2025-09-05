5886c5cabcb2461789bf71ce5d344c5be8b31e6b
Matthew Younger <matthewyounger33@gmail.com>
2025-09-05 00:02:03 -0500

    week 3 notes/submission

---
+++ week-3/src/ofugsnuid.py
@@ -0,0 +1,14 @@
+# [[file:../lecture.org::ofugsnuid_py][ofugsnuid_py]]
+# [[file:lecture.org::ofugsnuid_py][pledge_py]]
+# Matthew Younger (pledged)
+# pledge_py ends here
+
+
+num_ints = int(input())
+nums = []
+for i in range(num_ints):
+    nums.append(input())
+
+for line in nums[::-1]:
+    print(line)
+# ofugsnuid_py ends here
